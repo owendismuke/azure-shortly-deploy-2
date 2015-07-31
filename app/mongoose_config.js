b5287767dfb6720bee4c20218da9f8da554af2c4
Owen Dismuke <owen.dismuke@gmail.com>
2015-07-30 19:18:06 -0700

    ...

--- app/mongoose_config.js
+++ app/mongoose_config.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var uri = process.env.MONGOLAB_URI || 'mongodb://localhost/shortlydb';
 mongoose.connect(uri);
 
+
 var db = mongoose.connection;
 
 module.exports = db;
